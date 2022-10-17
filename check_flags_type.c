7828763a3453ae80d8c83da5e84436b5d8b766ca
Alabi Emmanuel Oluwatimileyin <eoalabi@student.oauife.edu.ng>
2022-10-17 08:51:44 -0700

    checks flag type

---
+++ check_flags_type.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * check_flag - check for flags
+ * @c: Flag passed
+ *
+ * Return: 1 - if present
+ *         0 - not present
+ */
+int check_flag(char c)
+{
+	char *s = "+# ";
+	int i;
+
+	if (c == '%')
+		return (1);
+	for (i = 0; s[i]; i++)
+	{
+		if (c == s[i])
+			return (1);
+	}
+	return (0);
+}
+
+
